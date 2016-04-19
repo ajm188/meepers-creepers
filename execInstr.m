c48787601ce17861ce874634cb4f4acc9e534c0a
Cameron Gutman <aicommander@gmail.com>
2016-04-18 23:35:48 -0400

    Move instruction parsing functions into execInstr.m

--- execInstr.m
+++ execInstr.m
@@ -101,3 +101,36 @@ switch (opCode(instr))
         return;
 end
 end
+
+function ft = funct(instr)
+%FUNCT Return the funct of an instruction. This is the last six bits.
+%      Instr MUST be an R-TYPE instruction. R-instructions are the only
+%      instructions with a funct.
+ft = bin2int(bitget(instr, 6:-1:1));
+end
+
+function type = instrType(instr)
+%INSTRTYPE Returns the type of instruction, as defined by the enum in
+%          InstructionType.
+
+% I-TYPE opcodes are 4, 5, 8-A, C, D, F, 20, 21, 23-25, 28, 2B in hex
+i_type_opcodes = [4:5, 8:10, 12:13, 15, 32:33, 35:37, 40, 43];
+j_type_opcodes = [2 3];
+
+op = opCode(instr);
+if op == 0
+    type = InstructionType.R;
+elseif ~isempty(find(i_type_opcodes == op, 1))
+    type = InstructionType.I;
+elseif ~isempty(find(j_type_opcodes == op, 1))
+    type = InstructionType.J;
+else
+    type = InstructionType.UNKNOWN;
+end
+end
+
+function op = opCode(instr)
+%OPCODE Return the op code of an instruction.
+%       The op code is the first six bits.
+op = bin2int(bitget(instr, 32:-1:27));
+end
--- funct.m
+++
@@ -1,6 +0,0 @@
-function ft = funct(instr)
-%FUNCT Return the funct of an instruction. This is the last six bits.
-%      Instr MUST be an R-TYPE instruction. R-instructions are the only
-%      instructions with a funct.
-ft = bin2int(bitget(instr, 6:-1:1));
-end
