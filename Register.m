0266c403b1e3c72d1d86fb1813d65d350cce9600
Cameron Gutman <aicommander@gmail.com>
2016-04-19 01:37:09 -0400

    Add the same + 1 offset to properties in the Register class

--- Register.m
+++ Register.m
@@ -1,49 +1,48 @@
 classdef Register
-    
+
+    % See the note in execInstr for why we add 1 to each index.
     properties (Constant)
-        % put $zero at index 32, so that the indexing for the rest of the
-        % registers is consistent with the spec.
-        zero = 32;
+        zero = 0 + 1;
         
-        at = 1;
+        at = 1 + 1;
         
-        v0 = 2;
-        v1 = 3;
+        v0 = 2 + 1;
+        v1 = 3 + 1;
         
-        a0 = 4;
-        a1 = 5;
-        a2 = 6;
-        a3 = 7;
+        a0 = 4 + 1;
+        a1 = 5 + 1;
+        a2 = 6 + 1;
+        a3 = 7 + 1;
         
-        t0 = 8;
-        t1 = 9;
-        t2 = 10;
-        t3 = 11;
-        t4 = 12;
-        t5 = 13;
-        t6 = 14;
-        t7 = 15;
-        t8 = 24;
-        t9 = 25;
+        t0 = 8 + 1;
+        t1 = 9 + 1;
+        t2 = 10 + 1;
+        t3 = 11 + 1;
+        t4 = 12 + 1;
+        t5 = 13 + 1;
+        t6 = 14 + 1;
+        t7 = 15 + 1;
+        t8 = 24 + 1;
+        t9 = 25 + 1;
         
-        s0 = 16;
-        s1 = 17;
-        s2 = 18;
-        s3 = 19;
-        s4 = 20;
-        s5 = 21;
-        s6 = 22;
-        s7 = 23;
-        s8 = 30;
+        s0 = 16 + 1;
+        s1 = 17 + 1;
+        s2 = 18 + 1;
+        s3 = 19 + 1;
+        s4 = 20 + 1;
+        s5 = 21 + 1;
+        s6 = 22 + 1;
+        s7 = 23 + 1;
+        s8 = 30 + 1;
         
-        k0 = 26;
-        k1 = 27;
+        k0 = 26 + 1;
+        k1 = 27 + 1;
         
-        gp = 28;
+        gp = 28 + 1;
         
-        sp = 29;
+        sp = 29 + 1;
         
-        ra = 31;
+        ra = 31 + 1;
     end
     
     methods (Access = private)
