11fe826532c88093da1f3319e7fb2289534b3932
Cameron Gutman <aicommander@gmail.com>
2016-04-21 02:19:45 -0400

    Fix off by one in readBuffer

--- readBuffer.m
+++ readBuffer.m
@@ -4,4 +4,4 @@ function data = readBuffer(cpustate, address, length)
 
 pageData = cpustate.pages{pageIndex}.data;
 
-data = pageData(pageOffset:pageOffset+length);
+data = pageData(pageOffset:pageOffset+length-1);
